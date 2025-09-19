191d787e3f51fb90dacc6c58e2b7f34949d1c0e2
joyangjin <wndid2008@gmail.com>
2025-09-19 14:15:06 +0900

    add testCode, Mockito 사용

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -44,7 +44,7 @@ public class ProductService {
         int myprice = requestDto.getMyprice();
         if (myprice < MIN_MY_PRICE) {
             throw new IllegalArgumentException(
-                "유효하지 않은 관심가격입니다. 최소" + MIN_MY_PRICE + "원 이상으로 설정해주세요.");
+                "유효하지 않은 관심가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해주세요.");
         }
 
         Product product = productRepository.findById(id).orElseThrow(() ->
